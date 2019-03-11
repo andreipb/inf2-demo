84f7c159d36d2d6a918d7981f0b7db5a1e92f591
Andrei Popescu-Belis <andreipb@users.noreply.github.com>
2019-03-11 11:39:18 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "maximum.h"
+#include <cstdlib>
+#include <iostream>
+#include <typeinfo>
+
+
+using namespace std;
+
+int main() {
+    {
+        float a = 3.f;
+        float b = 2.f;
+        cout << "Maximum de a et b : " << maximum(a, b)
+                << " de type : " << typeid (maximum(a, b)).name() << endl;
+    }
+    return EXIT_SUCCESS;
+}
