39f83b4e72113953df0ebeba9efbe991a7ed0708
Andrei Popescu-Belis <andreipb@users.noreply.github.com>
2019-03-11 11:55:33 +0100

    Add cout message for debugging.

--- maximum.h
+++ maximum.h
@@ -1,7 +1,10 @@
 #ifndef MAXIMUM_H
 #define MAXIMUM_H
 
+#include <iostream>
+
 template <typename T> T maximum(T x, T y) {
+    std::cout << "Appel de maximum" << std::endl;
     return (x>y)?x:y;
 }
 
