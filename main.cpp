ff6bdc3af89be67a940ca769608befdf1e7c65b2
Andrei Popescu-Belis <andreipb@free.fr>
2019-04-01 12:13:34 +0200

    changed var a in main.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     {
-        float a = 3.f;
+        float a = 4.f;
         float b = 2.f;
         cout << "Maximum de a et b : " << maximum(a, b)
                 << " de type : " << typeid (maximum(a, b)).name() << endl;
